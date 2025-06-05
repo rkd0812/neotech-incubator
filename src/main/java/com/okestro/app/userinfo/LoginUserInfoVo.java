e055721062674bfd63d2f0b713055bc19b4f4e13
Lim Dong Kyo <myppy2628@gmail.com>
2025-06-05 12:47:20 +0900

    1. interceptor 디렉토리 위치 변경 app → framework 2. LoginUserInfoVo 추가 3. LoginUserInfo 관련 sql 쿼리 추가

--- src/main/java/com/okestro/app/userinfo/LoginUserInfoVo.java
+++ src/main/java/com/okestro/app/userinfo/LoginUserInfoVo.java
@@ -1,12 +1,10 @@
 package com.okestro.app.userinfo;
 import com.okestro.app.cmmn.vo.BaseVo;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class LoginUserInfoVo extends BaseVo {
     private String userEmail;
     private String userPassword;
